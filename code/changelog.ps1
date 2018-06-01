0439c2957a40f055686d57075c4b2f5812d497de
silvan.pfister <silvan.pfister@hotmail.com>
2018-06-01 16:18:28 +0200

    Fixes
    Script now works on other folders than it's root.

--- code/changelog.ps1
+++ code/changelog.ps1
@@ -3,17 +3,16 @@ $changelogversion = "clv1.0"
 
 $dateFormat = "yyyy-MM-dd-HH-mm-ss-ffff";
 
-$log = "clv1.0`n$source`n$(Get-Date -Format $dateFormat )" 
+$log = "$changelogversion`n$source`n$(Get-Date -Format $dateFormat )" 
 
 function Recourse-ChangeLog-Folders ([string]$folder) {
-	$log = "$folder`nSUB"
+	$log = ""
 	foreach ($file in Get-ChildItem $folder -File ) {
 		$log = "$log`n`"$file`" $(Get-Date $file.LastWriteTime -Format $dateFormat )"
 	}
 	foreach ($sub in Get-ChildItem $folder -Directory) {
-		$log = "$log $(Recourse-ChangeLog-Folders $sub)"
+		$log = "$log`n`"$sub`"`nSUB$(Recourse-ChangeLog-Folders "$folder\$sub")`nEND"
 	}
-	$log = "$log`nEND"
 	return $log
 }
 
@@ -22,7 +21,9 @@ foreach ($file in Get-ChildItem $source -File ) {
 	$log = "$log`n`"$file`" $(Get-Date $file.LastWriteTime -Format $dateFormat )"
 }
 foreach ($sub in Get-ChildItem $source -Directory ) {
-	$log = "$log`n$(Recourse-ChangeLog-Folders $sub)"
+	$log = "$log`n`"$sub`"`nSUB$(Recourse-ChangeLog-Folders "$source\$sub")`nEND"
 }
 
-$log > .changelog
+$log > "$source\.changelog"
+
+Write-Output "success`noutput: $Source\.changelog"
