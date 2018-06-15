2a6e84ba2ee174e9b739f38a16d316f1733b005b
david.buetikofer <david.f.buetikofer@gmail.com>
2018-06-15 13:26:47 +0200

    shit script that doesnt work

---
+++ app.ps1
@@ -0,0 +1,3 @@
+. "$PSScriptRoot/code/getFolder.ps1"
+
+getFolder 'https://github.com/Globidolf/M122/blob/LB1/code/' "C:$env:HOMEPATH/Downloads/code"
--- code/getFolder.ps1
+++ code/getFolder.ps1
@@ -1,7 +1,34 @@
-param([string]$source, [string]$target)
+<#param([string]$source, [string]$target)
 .\code\getFile.ps1 "$source\.changelog" "$target\.templog"
 
 foreach($line in Get-Content "$target\.templog"){
-    $File = #get filename from line
+    $File = 
     .\code\getFile.ps1 "$source\$File" "$target\$File"
+}#>
+
+function getFolder ([string]$url, [string]$output) {
+    #$wc = New-Object System.Net.WebClient
+    #$wc.DownloadFile($folder, $destination)
+    $uri = [System.Uri]($url)
+    $directoryContentsList = Invoke-WebRequest -Uri $uri
+
+    $uris = foreach($link in $directoryContentsList.Links.href) {
+        $link_uri = [System.URI]($link)
+
+        if($link_uri.IsAbsoluteUri) {
+            $link_uri
+        } else {
+            if([System.Uri]::TryCreate($uri, $link,[ref]$link_uri)) {
+                $link_uri
+            }
+        }
+    }
+
+    foreach($item in $uris) {
+        if( $uri.Authority -eq $item.Authority ) {
+            $file = $item.segments[-1]
+            Write-Host $file
+            Invoke-WebRequest -Uri $item -OutFile $file
+        }
+    }
 }
